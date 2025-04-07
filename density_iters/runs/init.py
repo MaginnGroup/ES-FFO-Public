1aeec2ede22f67415d02e6039872de94f9d3769f
Montana Carlozo <mcarlozo@nd.edu>
2025-04-07 17:43:44 -0400

    Updated init.py file to include actual solvent classes

--- density_iters/runs/init.py
+++ density_iters/runs/init.py
@@ -3,85 +3,26 @@ import numpy as np
 import unyt as u
 from scipy.stats import qmc
 import pandas as pd
-import csv
+import sys
 import glob
+import os
 
-from fffit.utils import values_scaled_to_real
-from utils.molec_class_files import (
-    r14,
-    r32,
-    r50,
-    r125,
-    r134a,
-    r143a,
-    r170,
-    r41,
-    r23,
-    r161,
-    r152a,
-    r152,
-    r134,
-    r143,
-    r116,
-)
+sys.path.append("../..")
+from utils.molec_class_files import esolvs
+from fffit.fffit.utils import values_scaled_to_real
 
-# Load class properies for each training and testing molecule
-R14 = r14.R14Constants()
-R32 = r32.R32Constants()
-R50 = r50.R50Constants()
-R125 = r125.R125Constants()
-R134a = r134a.R134aConstants()
-R143a = r143a.R143aConstants()
-R170 = r170.R170Constants()
-R41 = r41.R41Constants()
-R23 = r23.R23Constants()
-R161 = r161.R161Constants()
-R152a = r152a.R152aConstants()
-R152 = r152.R152Constants()
-R143 = r143.R143Constants()
-R134 = r134.R134Constants()
-R116 = r116.R116Constants()
+sys.path.remove("../..")
 
-molec_dict = {
-    "R14": R14,
-    "R32": R32,
-    "R50": R50,
-    "R125": R125,
-    "R134a": R134a,
-    "R143a": R143a,
-    "R170": R170,
-    "R41": R41,
-    "R23": R23,
-    "R161": R161,
-    "R152a": R152a,
-    "R152": R152,
-    "R143": R143,
-    "R134": R134,
-    "R116": R116,
-}
+from utils.molec_class_files import esolvs
 
-
-def _get_molec_dicts():
-    # Load class properies for each molecule
-    from utils.molec_class_files import r41  # import all the class files
-
-    R41 = r41.R41Constants()
-
-    # Create a dictionary with all of the data
-    molec_dict = {
-        "R41": R41,
-    }
-    return molec_dict
-
-
-def _get_class_from_molecule(molecule_name):
-    molec_dict = _get_molec_dicts()
-    return {molecule_name: molec_dict[molecule_name]}
+# Load class properies for each training molecule
+mol_names = ["EG", "Gly", "ACN", "MeOH", "DMSO", "THF", "DCM", "DEC"]
+molec_dict = esolvs.make_dict(mol_names)
 
 
 def unpack_molec_values(class_data, state_point, sample):
     """
-    Unpacks sckaled sample values given the molecule under study
+    Unpacks scaled sample values given the molecule under study
     """
     param_names = class_data.param_names
     for i, param in enumerate(param_names):
@@ -99,12 +40,13 @@ def unpack_molec_values(class_data, state_point, sample):
 def determine_density_iter(molec_name):
     # Check the analysis folder for analysis/MolName/density-iter-X folders
     # Find the highest density-iter-X folder
-    files = sorted(glob.glob("analysis/" + molec_name + "/dens-iter-*"))
+    files = sorted(glob.glob("../analysis/" + molec_name + "/dens-iter-*"))
     if len(files) == 0:
         dens_iter = 1
     else:
-        # Get the highest density-iter-X folder from the last character of the last file
-        dens_iter = files[-1][-1]
+        # Get the highest density-iter-X folder from the last character of the last file minus the .csv part
+        base = os.path.splitext(os.path.basename(files[-1]))[0]
+        dens_iter = base[-1]
     return dens_iter
 
 
@@ -127,21 +69,19 @@ def init_project():
 
         # Use GenLHS samples to generate LHS samples in the analysis folder
         # Load the lhs_samples and bounds
-        class_dict = _get_class_from_molecule(job.sp.mol_name)
-        class_data = class_dict[job.sp.mol_name]
-        bounds = class_data.param_bounds
+        bounds = molec_data.param_bounds
         lhs_samples = pd.read_csv(
-            "analysis/" + molec_name + "/dens-iter-" + str(dens_iter) + ".csv"
+            "../analysis/" + molec_name + "/dens-iter-" + str(dens_iter) + ".csv",
+            index_col=0,
         )
-
         # Convert scaled latin hypercube samples to physical values
         scaled_params = values_scaled_to_real(lhs_samples, bounds)
 
         # Define temps (from constants files)
         temps = list(molec_data.expt_Pvap.keys())
         for temp in temps:
-            liq_density = molec_data.expt_liq_density[int(temp)]
-            vap_density = molec_data.expt_vap_density[int(temp)]
+            liq_density = molec_data.expt_liq_density[temp]
+            vap_density = molec_data.expt_vap_density[temp]
             avg_density = (liq_density + vap_density) / 2
             for sample in scaled_params:
                 # Define the state point w/ unchanging characteristics
@@ -149,8 +89,8 @@ def init_project():
                     "mol_name": molec_name,
                     "dens-iter": dens_iter,
                     "smiles": molec_data.smiles_str,
-                    "T": float(temp.in_units(u.K).value),
-                    "P": float(class_data.expt_Pvap[temp]),
+                    "T": float((temp * u.K).in_units(u.K).value),
+                    "P": float(molec_data.expt_Pvap[temp]),
                     "rho_avg": avg_density,  # kg/m^3
                     "nsteps_nvt1": nsteps_nvt1,
                     "nsteps_npt": nsteps_npt,
@@ -158,10 +98,13 @@ def init_project():
                     "nsteps_intereq": nsteps_intereq,
                     "nsteps_interprod": nsteps_interprod,
                 }
-                state_point = unpack_molec_values(class_data, state_point, sample)
 
-                job = project.open_job(state_point)
-                job.init()
+                state_point = unpack_molec_values(molec_data, state_point, sample)
+
+                # print(state_point)
+
+                # job = project.open_job(state_point)
+                # job.init()
 
 
 if __name__ == "__main__":
