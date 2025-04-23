7681cd20ce23dcde96b9f53155db1b26c0597726
Montana Carlozo <mcarlozo@nd.edu>
2025-04-23 15:16:37 -0400

    generated acn 298.15 as a test case

--- density_iters/init.py
+++ density_iters/init.py
@@ -84,7 +84,7 @@ def init_project():
 
         # Define temps (from constants files)
         temps = list(molec_data.expt_Pvap.keys())
-        for temp in [temps[0]]:
+        for temp in [temps[1]]:
             liq_density = molec_data.expt_liq_density[temp]
             # vap_density = molec_data.expt_vap_density[temp]
             # rho_avg = (liq_density + vap_density) / 2.0
