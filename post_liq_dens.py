9dfdeabf83130683f6f09e732ca370224ec54ebc
Montana Carlozo <mcarlozo@nd.edu>
2025-04-24 17:54:31 -0400

    made notebook to test post_liq_dens

--- post_liq_dens.py
+++ post_liq_dens.py
@@ -1,11 +1,13 @@
 import signac
 import sys
-
+from utils.molec_class_files import esolvs
 from utils.analyze_iters import save_signac_results, find_new_samples, find_pareto, plot_gp_examples
 
 #Set iters to analyze and properties to analyze
 iters = [1]  # Change me as needed
 property_names = ["density", "surf_tens"]  # Change me as needed
+mol_names = ["EG", "Gly", "ACN", "MeOH", "DMSO", "THF", "DCM", "DEC", "DMF"] # Change me as needed
+
 
 #Set seeds and preferences
 cl_shuffle_seed = 1  # classifier
@@ -21,11 +23,12 @@ verbose = True
 #Get Project
 base_name = "density_iters/runs/"
 project = signac.get_project(base_name)
+molec_dict = esolvs.make_dict(mol_names)
 
 # Save DataFrame of all molecule data for each iteration
-df_all_molec = save_signac_results(project, property_names, save_csv)
-all_prop_models = plot_gp_examples(df_all_molec, gp_shuffle_seed, save_fig)
-next_samples = find_new_samples(df_all_molec, verbose, save_fig, cl_shuffle_seed, gp_shuffle_seed, dist_seed)
+df_all_molec = save_signac_results(project, molec_dict, property_names, save_csv)
+all_prop_models = plot_gp_examples(df_all_molec, molec_dict, gp_shuffle_seed, save_fig)
+next_samples = find_new_samples(df_all_molec, molec_dict, verbose, save_fig, cl_shuffle_seed, gp_shuffle_seed, dist_seed)
 if max(iters) > 1:
     all_final_params = find_pareto(df_all_molec)
     for key, value in all_final_params.items():
