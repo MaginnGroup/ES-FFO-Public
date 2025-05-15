48ef14ce54b63d79258f93f23fac697c2115a6ae
Montana Carlozo <mcarlozo@nd.edu>
2025-05-15 14:58:59 -0400

    Updated_init_opt_ff_ms

--- Opt_ES/init_optff_ms.py
+++ Opt_ES/init_optff_ms.py
@@ -4,64 +4,20 @@ import unyt as u
 import sys
 import pandas as pd
 
-from utils.molec_class_files import (
-    r14,
-    r32,
-    r50,
-    r125,
-    r134a,
-    r143a,
-    r170,
-    r41,
-    r23,
-    r161,
-    r152a,
-    r152,
-    r134,
-    r143,
-    r116,
-)
+sys.path.append("..")
+from utils.molec_class_files import esolvs
 from utils import atom_type, opt_atom_types
+sys.path.remove("..")
 
-at_numbers = [1,2,6,8] #1,2,6,8
+# Load class properies for each training molecule
+mol_names = ["EG" , "Gly", "ACN", "MeOH", "DMSO", "THF", "DCM", "DEC", "DMF"]
+molec_dict = esolvs.make_dict(mol_names)
+
+at_numbers = [1,2] #1,2
 num_restarts = 3  # Number of restarts for replications
 n_vap = 160  # number of molecules in vapor phase
 n_liq = 640
 obj_choice = "ExpVal"  # Objective to consider
-
-R14 = r14.R14Constants()
-R32 = r32.R32Constants()
-R50 = r50.R50Constants()
-R125 = r125.R125Constants()
-R134a = r134a.R134aConstants()
-R143a = r143a.R143aConstants()
-R170 = r170.R170Constants()
-R41 = r41.R41Constants()
-R23 = r23.R23Constants()
-R161 = r161.R161Constants()
-R152a = r152a.R152aConstants()
-R152 = r152.R152Constants()
-R143 = r143.R143Constants()
-R134 = r134.R134Constants()
-R116 = r116.R116Constants()
-
-molec_dict = {
-    "R14": R14,
-    "R32": R32,
-    "R50": R50,
-    "R125": R125,
-    "R134a": R134a,
-    "R143a": R143a,
-    "R170": R170,
-    "R41": R41,
-    "R23": R23,
-    "R161": R161,
-    "R152a": R152a,
-    "R152": R152,
-    "R143": R143,
-    "R134": R134,
-    "R116": R116,
-}
 
 # Initialize project
 project = signac.init_project("opt_ff_ms")
@@ -80,16 +36,6 @@ def unpack_molec_values(molec_name, at_class, sample, state_point):
     # Add params based on the order they show up in given the mapping
     for param in param_names:
         state_point[param] = sample[index_mapping[param]].item()
-
-    #For R23 AT Scheme 2, use the GAFF parameters for H since these parameters aren't identifiable
-    if molec_name == "R23" and at_class.scheme_name == "at_02":
-        state_point["epsilon_H1"] = float(7.901 * (u.K * u.kb).in_units("kJ/mol"))
-        state_point["sigma_H1"] = float((2.115 * u.Angstrom).in_units(u.nm).value)
-    # For R41 and R23 add the GAFF Parameter for epsilon C1 instead of the OptFF one
-    # if molec_name in ["R41", "R23"]:
-    #     state_point["epsilon_C1"] = float(55.052 * (u.K * u.kb).in_units("kJ/mol"))
-    #     state_point["GAFF_epsilon_C1"] = True
-    # print(state_point)
     return state_point
 
 
@@ -103,18 +49,8 @@ for molec_name, molec_data in molec_dict.items():
 
     # Load sample from best set using ExpVal and all training molecules
     save_data = False
-    molec_names = [
-        "R14",
-        "R32",
-        "R50",
-        "R170",
-        "R125",
-        "R134a",
-        "R143a",
-        "R41",
-    ]  # Training data to consider
-    # molec_data_dict = {"R14":R14, "R32":R32, "R50":R50, "R170":R170, "R125":R125, "R134a":R134a, "R143a":R143a}
-    # all_gp_dict = opt_atom_types.get_gp_data_from_pkl(list(molec_data_dict.keys()))
+    molec_names = mol_names  # Training data to consider
+
     for at_number in at_numbers:
         setup = opt_atom_types.Problem_Setup(molec_names, at_number, obj_choice)
         all_molec_dir = setup.use_dir_name
