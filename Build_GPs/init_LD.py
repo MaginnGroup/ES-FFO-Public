4fa7d6e2d8434b6d1055c03ff7973531963c2b2d
Montana Carlozo <mcarlozo@nd.edu>
2025-05-20 16:58:29 -0400

    Made new LD iter tests

--- Build_GPs/init_LD.py
+++ Build_GPs/init_LD.py
@@ -7,13 +7,15 @@ import sys
 import glob
 import os
 
-sys.path.append("..")
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if root_dir not in sys.path:
+    sys.path.insert(0, root_dir)
+
 from utils.molec_class_files import esolvs
 from fffit.fffit.utils import values_scaled_to_real
-sys.path.remove("..")
 
 # Load class properies for each training molecule
-mol_names = ["R125"] #["EG" , "Gly", "ACN", "MeOH", "DMSO", "THF", "DCM", "DEC", "DMF"]
+mol_names = ["EG" , "Gly", "MeOH"] #["EG" , "Gly", "ACN", "MeOH", "DMSO", "THF", "DCM", "DEC", "DMF"]
 molec_dict = esolvs.make_dict(mol_names)
 
 def calc_nmols(sp):
@@ -23,7 +25,6 @@ def calc_nmols(sp):
     density = sp["rho_liq"]
     #Calculatue box lengths from system density and 3.0*cutoff (3.6 = 3.0*1.2 A)
     xy_len = 3.6
-    # new_V = sp["aspect_ratio"]*xy_len**3 #Rectangular box
     new_V = xy_len**3 #Square box
     #Calculate the number of molecules from the new volume and the given density
     nmols = int(np.floor(density*1000*6.022*1e23*new_V/(sp["mol_wt"]*1e27)))
@@ -67,17 +68,8 @@ def determine_iter(molec_name):
 
 
 nsteps_nvt_eq = 100000  # 100ps
-nsteps_npzzat_eq = 500000  # 5 ns
-nsteps_npzzat_prod = 2500000  # 10 ns
-nsteps_fl_eq = 100000  # 100ps
-nsteps_npt_pre_eq = 500000  # 500ps
 nsteps_npt_eq = 500000  # 500ps (minimum)
 nsteps_npt_prod = 2500000  # 2.5 ns
-nsteps_nvt_prod = 100000  # 100 ps
-nsteps_intereq = 30000000  # 15 ns (minimum)
-nsteps_interprod = 40000000  # 30 ns
-# n_particles = 10000  # Number of particles in the system
-# nmols = 1000  # Number of molecules in the system
 aspect_ratio = 3.0  # Aspect ratio of the box
 
 
@@ -85,7 +77,6 @@ def init_project():
     # Loop over all molecules
     for molec_name, molec_data in molec_dict.items():
         # Determine Density iter based off of the analysis folder
-        # For now use dens-iter = 1
         dens_iter = determine_iter(molec_name)
 
         # Initialize project
@@ -101,19 +92,19 @@ def init_project():
         # Convert scaled latin hypercube samples to physical values
         scaled_params = values_scaled_to_real(lhs_samples, bounds)
         #Make the GAFF param_set (test)
-        scaled_params = molec_data.A_kJmol_to_nm_Kkb(molec_data.gaff_params)
-        scaled_params = np.array(list(scaled_params.values())).reshape(1,-1)
-        # nmols = int(n_particles/molec_data.n_atoms) #Number of molecules in the system
+        # scaled_params = molec_data.A_kJmol_to_nm_Kkb(molec_data.gaff_params)
+        # scaled_params = np.array(list(scaled_params.values())).reshape(1,-1)
+
         # Define temps (from constants files)
         temps = list(molec_data.expt_Pvap.keys())
-        for temp in [temps[-3]]:
+        for temp in temps:
             liq_density = molec_data.expt_liq_density[temp]
             vap_density = molec_data.expt_vap_density[temp]
             max_vd = molec_data.expt_vap_density[max(temps)]
             min_ld = molec_data.expt_liq_density[max(temps)]
             rho_thresh = (max_vd + min_ld)/2.0
             rho_avg = (liq_density + vap_density) / 2.0
-            for sample in scaled_params[0].reshape(1, -1):
+            for sample in scaled_params[:3]:
                 # Define the state point w/ unchanging characteristics
                 state_point = {
                     "mol_name": molec_name,
@@ -123,31 +114,22 @@ def init_project():
                     "P": float(molec_data.expt_Pvap[temp]),  # bar
                     "rho_liq": liq_density,  # kg/m^3
                     "rho_thresh": rho_thresh,  # kg/m^3
-                    # "rho_avg": rho_avg,  # kg/m^3
                     "mol_wt": molec_data.molecular_weight,  # g/mol
-                    # "nmols": nmols,  # Number of molecules
                     "aspect_ratio": aspect_ratio,  # Aspect ratio of the box
                     "nsteps_nvt_eq": nsteps_nvt_eq,
-                    # "nsteps_npzzat_eq": nsteps_npzzat_eq,
-                    # "nsteps_npzzat_prod": nsteps_npzzat_prod,
-                    # "nsteps_fl_eq": nsteps_fl_eq,
-                    # "nsteps_npt_pre_eq": nsteps_npt_pre_eq,
                     "nsteps_npt_eq": nsteps_npt_eq,
                     "nsteps_npt_prod": nsteps_npt_prod,
-                    # "nsteps_nvt_prod": nsteps_nvt_prod,
-                    "nsteps_intereq": nsteps_intereq,
-                    "nsteps_interprod": nsteps_interprod,
                     "max_sigma" : np.max(molec_data.bounds_sig)
                 }
 
                 state_point, max_sigma = unpack_molec_values(molec_data, state_point, sample)
                 state_point, nmols = calc_nmols(state_point)
                 state_point["nmols"] = nmols
+                #Optionally set max_sigma to the max sigma value from the FF rather than the max from the bounds
                 # state_point["max_sigma"] = max_sigma
 
                 job = project.open_job(state_point)
                 job.init()
-
 
 if __name__ == "__main__":
     init_project()
