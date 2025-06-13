588b199968a099da49069f75280fb1946a880245
Montana Carlozo <mcarlozo@nd.edu>
2025-06-13 17:31:43 -0400

    Updated Iter 1 results for all iters- Decided tob rerun MeOH jobs based on changes to classifier

--- Build_GPs/ld_iters/del_jobs.py
+++ Build_GPs/ld_iters/del_jobs.py
@@ -7,14 +7,16 @@ import shutil
 project = signac.get_project()
 
 #Loop over all jobs in the project
-for job in project:
-    # Check if ld_fail exists in the job document
-    if "ld_fail" in job.document:
-        for fname in os.listdir(job.path):
-            if fname != "signac_statepoint.json":
-                fpath = os.path.join(job.path, fname)
-                if os.path.isfile(fpath) or os.path.islink(fpath):
-                    os.remove(fpath)
-                elif os.path.isdir(fpath):
-                    shutil.rmtree(fpath)
-                    print(f"Skipping directory: {fpath}")
+for job in project.find_jobs({"mol_name":"MeOH", "iter": 2}):
+    job.remove()  # Remove the job from the project
+# for job in project:
+#     # Check if ld_fail exists in the job document
+#     if "ld_fail" in job.document:
+#         for fname in os.listdir(job.path):
+#             if fname != "signac_statepoint.json":
+#                 fpath = os.path.join(job.path, fname)
+#                 if os.path.isfile(fpath) or os.path.islink(fpath):
+#                     os.remove(fpath)
+#                 elif os.path.isdir(fpath):
+#                     shutil.rmtree(fpath)
+#                     print(f"Skipping directory: {fpath}")
