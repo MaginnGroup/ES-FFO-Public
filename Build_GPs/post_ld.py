a962913510bc8783cad94cd3f4bf6152e2a05021
Montana Carlozo <mcarlozo@nd.edu>
2025-07-02 15:47:43 -0400

    Track failed jobs with vle find jobs and for post_ld check mse 10 before building models

--- Build_GPs/post_ld.py
+++ Build_GPs/post_ld.py
@@ -3,7 +3,9 @@ import sys
 import os
 from pathlib import Path
 
-root_path = Path(__file__).resolve().parents[1]  # ES-FFO directory (two levels up from this script)
+root_path = (
+    Path(__file__).resolve().parents[1]
+)  # ES-FFO directory (two levels up from this script)
 if str(root_path) not in sys.path:
     sys.path.insert(0, str(root_path))
 
@@ -20,10 +22,20 @@ print(f"Script location: {Path(__file__).parent}")
 # #Set iters to analyze and properties to analyze
 # iters = [1]  # Change me as needed
 property_names = ["liq_density"]  # Change me as needed
-mol_names = ["EG", "Gly", "ACN", "MeOH", "DMSO", "THF", "DCM", "DEC", "DMF"] # Change me as needed
+mol_names = [
+    "EG",
+    "Gly",
+    "ACN",
+    "MeOH",
+    "DMSO",
+    "THF",
+    "DCM",
+    "DEC",
+    "DMF",
+]  # Change me as needed
 
 
-#Set seeds and preferences
+# Set seeds and preferences
 cl_shuffle_seed = 1  # classifier
 gp_shuffle_seed = 42  # GP seed
 dist_seed = 1  # Distance seed
@@ -35,7 +47,7 @@ verbose = True
 
 ##############################################################################
 ##############################################################################
-#Get Project
+# Get Project
 iter_type = "ld_iters"
 project = signac.get_project(iter_type)
 molec_dict = esolvs.make_dict(mol_names)
@@ -43,16 +55,30 @@ molec_dict = esolvs.make_dict(mol_names)
 # Save DataFrame of all molecule data for each iteration
 df_all_molec = get_signac_results(project, molec_dict, property_names)
 df_all_molec = save_signac_results(df_all_molec, iter_type, save_csv)
-#Make and save best GP models for all molecules and properties and plot GP examples
+
+# Check the MSE of the new samples
+mse_less10 = check_mse_10(
+    df_all_molec, molec_dict, mse_less_10_thresh, dist_seed, save_csv
+)
+
+# Make and save best GP models for all molecules and properties and plot GP examples
 models_molecs = get_best_models(df_all_molec, molec_dict, iter_type, gp_shuffle_seed)
 plot_gp_examples(df_all_molec, molec_dict, iter_type, gp_shuffle_seed, save_fig)
-#Check the MSE of the new samples
-mse_less10 = check_mse_10(df_all_molec, molec_dict, mse_less_10_thresh, dist_seed, save_csv)
-#Find the next samples to run if fewer than 25 samples have MSE less than 10
+
+# Find the next samples to run if fewer than 25 samples have MSE less than 10
 for key, value in mse_less10.items():
     if len(value) >= mse_less_10_thresh:
-        print(f"{key} : {len(value)} samples have MSE less than {mse_less_10_thresh}. Proceed to VLE.")
+        print(
+            f"{key} : {len(value)} samples have MSE less than {mse_less_10_thresh}. Proceed to VLE."
+        )
     else:
         print(f"{key} : Total samples with MSE less than {mse_less_10_thresh}, {value}")
-        next_samples = new_samples_ld(df_all_molec, molec_dict, verbose, save_fig, cl_shuffle_seed, gp_shuffle_seed, dist_seed)
-        
+        next_samples = new_samples_ld(
+            df_all_molec,
+            molec_dict,
+            verbose,
+            save_fig,
+            cl_shuffle_seed,
+            gp_shuffle_seed,
+            dist_seed,
+        )
--- Build_GPs/vle_iters/find_jobs.py
+++ Build_GPs/vle_iters/find_jobs.py
@@ -7,17 +7,17 @@ import shutil
 project = signac.get_project()
 
 count = 0
-#Loop over all jobs in the project
+# Loop over all jobs in the project
 for job in project.find_jobs({"mol_name": "MeOH", "iter": 1}):
     # Check if job document exists
     # print(f"Checking job {job.id}")
     if os.path.exists(job.fn("signac_job_document.json")):
         # if "eq_liq_dens" in job.document and job.doc["eq_liq_dens"] < 1.0:
         #     print(f"Job {job.id} has eq_liq_dens < 1.0: {job.doc['eq_liq_dens']}")
-        if "em_fin" in job.doc and "npzzat_eq_fin" not in job.doc:
+        if "ld_fail" in job.doc:
             count += 1
             print(f"Job {job.id}")
-            #Print the last line of the run_npt_prod.out log file
+            # Print the last line of the run_npt_prod.out log file
             # log_file = job.fn("run_npt_prod.out")
             # if os.path.exists(log_file):
             #     with open(log_file, "r") as f:
