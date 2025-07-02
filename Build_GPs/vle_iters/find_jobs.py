1fd914d8ca44d20637a813d21176b5df25a7e4ff
Montana Carlozo <mcarlozo@nd.edu>
2025-07-02 13:23:41 -0400

    Checked job progress

--- Build_GPs/vle_iters/find_jobs.py
+++ Build_GPs/vle_iters/find_jobs.py
@@ -8,22 +8,22 @@ project = signac.get_project()
 
 count = 0
 #Loop over all jobs in the project
-for job in project.find_jobs({"mol_name":"DMSO", "iter": 1}):
+for job in project.find_jobs({"mol_name": "MeOH", "iter": 1}):
     # Check if job document exists
     # print(f"Checking job {job.id}")
     if os.path.exists(job.fn("signac_job_document.json")):
         # if "eq_liq_dens" in job.document and job.doc["eq_liq_dens"] < 1.0:
         #     print(f"Job {job.id} has eq_liq_dens < 1.0: {job.doc['eq_liq_dens']}")
-        if "npt_prod_fin" not in job.document and "ld_fail" not in job.document:
+        if "em_fin" in job.doc and "npzzat_eq_fin" not in job.doc:
             count += 1
             print(f"Job {job.id}")
             #Print the last line of the run_npt_prod.out log file
-            log_file = job.fn("run_npt_prod.out")
-            if os.path.exists(log_file):
-                with open(log_file, "r") as f:
-                    lines = f.readlines()
-                    if lines:
-                        print(lines[-1].strip())
-            else:
-                print("Log file does not exist.")
+            # log_file = job.fn("run_npt_prod.out")
+            # if os.path.exists(log_file):
+            #     with open(log_file, "r") as f:
+            #         lines = f.readlines()
+            #         if lines:
+            #             print(lines[-1].strip())
+            # else:
+            #     print("Log file does not exist.")
 print(f"Total unfinished jobs: {count}")
