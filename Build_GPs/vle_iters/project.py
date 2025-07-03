58289a0fb6a7ec032aa668d1ce29b1681fcc8f1b
Montana Carlozo <mcarlozo@nd.edu>
2025-07-03 16:49:29 -0400

    Changed to cg method which converges more quickly

--- Build_GPs/vle_iters/project.py
+++ Build_GPs/vle_iters/project.py
@@ -1641,10 +1641,10 @@ def _generate_em_mdp(job):
     contents = """
 ; MDP file for energy minimization
 
-integrator	    = steep		    ; Algorithm (steep = steepest descent minimization)
+integrator	    = cg		    ; Algorithm (steep = steepest descent minimization)
 emtol		    = 100.0  	    ; Stop minimization when the maximum force < 100.0 kJ/mol/nm
 emstep          = 0.001          ; Energy step size
-; nstcgsteep      = 1000          ; Number of steps for the cg algorithm
+nstcgsteep      = 1000          ; Number of steps for the cg algorithm
 nsteps		    = 200000	  	    ; Maximum number of (minimization) steps to perform
 
 nstenergy                = 1000
