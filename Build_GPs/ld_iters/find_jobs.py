e7866d61949b78e830e2ee78642f7e0b2ed66fe0
Montana Carlozo <mcarlozo@nd.edu>
2025-07-06 01:08:07 -0400

    Added LD 5 results, prepared for Gly iter 6

--- Build_GPs/ld_iters/find_jobs.py
+++ Build_GPs/ld_iters/find_jobs.py
@@ -8,7 +8,7 @@ project = signac.get_project()
 
 count = 0
 #Loop over all jobs in the project
-for job in project.find_jobs({"mol_name":"DEC", "iter": 4}):
+for job in project.find_jobs({"mol_name":"Gly", "iter": 6}):
     # Check if job document exists
     # print(f"Checking job {job.id}")
     if os.path.exists(job.fn("signac_job_document.json")):
@@ -17,7 +17,7 @@ for job in project.find_jobs({"mol_name":"DEC", "iter": 4}):
         #     print(f"Job {job.id} has eq_liq_dens < 1.0: {job.doc['eq_liq_dens']}")
         if "npt_prod_fin" not in job.document and "ld_fail" not in job.document:
             count += 1
-            # print(f"Job {job.id}")
+            print(f"Job {job.id}")
             #Print the last line of the run_npt_prod.out log file
             log_file = job.fn("run_npt_prod.out")
             if os.path.exists(log_file):
@@ -25,7 +25,7 @@ for job in project.find_jobs({"mol_name":"DEC", "iter": 4}):
                 with open(log_file, "r") as f:
                     lines = f.readlines()
                     if lines:
-                        # if "Tue" not in lines[-1].strip():
+                        # if "Fri" not in lines[-1].strip():
                         print(f"Job {job.id}")
                         print(lines[-1].strip())
             # else:
