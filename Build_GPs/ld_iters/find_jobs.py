d631d23b4b4f5fd3af987064a50921c89eea9eb6
Montana Carlozo <mcarlozo@nd.edu>
2025-07-10 12:55:52 -0400

    Updated find jobs

--- Build_GPs/ld_iters/find_jobs.py
+++ Build_GPs/ld_iters/find_jobs.py
@@ -16,8 +16,8 @@ for job in project.find_jobs({"mol_name":"Gly", "iter": 7}):
         #     count +=1
         #     print(f"Job {job.id} has eq_liq_dens < 1.0: {job.doc['eq_liq_dens']}")
         if "npt_prod_fin" not in job.document and "ld_fail" not in job.document:
-            count += 1
-            print(f"Job {job.id}")
+            # count += 1
+            # print(f"Job {job.id}")
             #Print the last line of the run_npt_prod.out log file
             log_file = job.fn("run_npt_prod.out")
             if os.path.exists(log_file):
@@ -25,7 +25,7 @@ for job in project.find_jobs({"mol_name":"Gly", "iter": 7}):
                 with open(log_file, "r") as f:
                     lines = f.readlines()
                     if lines:
-                        if "Wed" not in lines[-1].strip() or "Thu" not in lines[-1].strip():
+                        if "Wed" not in lines[-1].strip() and "Thu" not in lines[-1].strip():
                             print(f"Job {job.id}")
                             print(lines[-1].strip())
             else:
