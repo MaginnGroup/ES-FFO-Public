dc8c86aea2bbd463d546aaf6794be8a829aa7d76
Montana Carlozo <mcarlozo@nd.edu>
2025-10-10 19:30:15 -0400

    Checked progress of DEC LD iter 1

--- Build_GPs/ld_iters/find_jobs.py
+++ Build_GPs/ld_iters/find_jobs.py
@@ -15,9 +15,9 @@ for job in project.find_jobs({"mol_name":"DEC", "iter": 1, "T": {"$in": [313.19,
         # if "eq_liq_dens" in job.document and job.doc["eq_liq_dens"] < 1.0:
         #     count +=1
         #     print(f"Job {job.id} has eq_liq_dens < 1.0: {job.doc['eq_liq_dens']}")
-        if "npt_eq_fin" not in job.document and "ld_fail" not in job.document:
+        if "npt_prod_fin" not in job.document and "ld_fail" not in job.document:
             count += 1
-            print(f"Job {job.id}")
+            # print(fs"Job {job.id}")
             #Print the last line of the run_npt_prod.out log file
             # log_file = job.fn("run_npt_prod.out")
             # if os.path.exists(log_file):
@@ -25,7 +25,7 @@ for job in project.find_jobs({"mol_name":"DEC", "iter": 1, "T": {"$in": [313.19,
             #     with open(log_file, "r") as f:
             #         lines = f.readlines()
             #         if lines:
-            #             if "Wed" not in lines[-1].strip() and "Thu" not in lines[-1].strip() and "Jul" in lines[-1].strip():
+            #             if "Sat" in lines[-1].strip():
             #                 print(f"Job {job.id}")
             #                 print(lines[-1].strip())
             # else:
