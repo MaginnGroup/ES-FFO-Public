6384ee98cc65fc31cfe982998698898a3ca75eed
Montana Carlozo <mcarlozo@nd.edu>
2025-10-20 20:25:16 -0400

    Added ld2 DEC results

--- Build_GPs/ld_iters/find_jobs.py
+++ Build_GPs/ld_iters/find_jobs.py
@@ -15,20 +15,20 @@ for job in project.find_jobs({"mol_name":"DEC", "iter": 2}):
         # if "eq_liq_dens" in job.document and job.doc["eq_liq_dens"] < 1.0:
         #     count +=1
         #     print(f"Job {job.id} has eq_liq_dens < 1.0: {job.doc['eq_liq_dens']}")
-        if "em_fin" not in job.document and "ld_fail" not in job.document:
+        if "npt_prod_fin" not in job.document and "ld_fail" not in job.document:
             count += 1
-            print(f"Job {job.id}")
+            # print(f"Job {job.id}")
             #Print the last line of the run_npt_prod.out log file
-            # log_file = job.fn("run_npt_prod.out")
-            # if os.path.exists(log_file):
-            #     # print(f"Job {job.id}")
-            #     with open(log_file, "r") as f:
-            #         lines = f.readlines()
-            #         if lines:
-            #             if "Wed" in lines[-1].strip():
-            #                 print(f"Job {job.id}")
-            #                 print(lines[-1].strip())
-            #                 # count += 1
-            # else:
-            #     print("Log file does not exist.")
+            log_file = job.fn("run_npt_prod.out")
+            if os.path.exists(log_file):
+                # print(f"Job {job.id}")
+                with open(log_file, "r") as f:
+                    lines = f.readlines()
+                    if lines:
+                        if "Mon" in lines[-1].strip():
+                            print(f"Job {job.id}")
+                            print(lines[-1].strip())
+                            # count += 1
+            else:
+                print("Log file does not exist.")
 print(f"Total unfinished jobs: {count}")
