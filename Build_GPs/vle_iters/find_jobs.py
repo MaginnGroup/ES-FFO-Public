46fca6fbc7c87dc06e8a4b0c7c548fd7a2e0788c
Montana Carlozo <mcarlozo@nd.edu>
2025-10-23 10:01:52 -0400

    Checking DEC VLE iters

--- Build_GPs/vle_iters/find_jobs.py
+++ Build_GPs/vle_iters/find_jobs.py
@@ -17,20 +17,21 @@ for job in group:
         count_running += 1
         # if "eq_liq_dens" in job.document and job.doc["eq_liq_dens"] < 1.0:
         #     print(f"Job {job.id} has eq_liq_dens < 1.0: {job.doc['eq_liq_dens']}")
-        if "npzzat_eq_fin" not in job.doc and "ld_fail" not in job.doc:
+        op_name = "inter_eq"
+        if f"{op_name}_fin" not in job.doc and "ld_fail" not in job.doc:
             count += 1
-            print(job.id)
+            # print(job.id)
             
-            # #Print the last line of the run_npt_prod.out log file
-            # log_file = job.fn("run_inter_prod.out")
-            # if os.path.exists(log_file):
-            #     with open(log_file, "r") as f:
-            #         lines = f.readlines()
-            #         if lines:
-            #             last_line = lines[-1].strip()
-            #             if "Fri" in last_line:
-            #                 print(last_line)
-            #                 print(f"Job {job.id}")
-            # else:
-            #     print("Log file does not exist.")
+            #Print the last line of the run_npt_prod.out log file
+            log_file = job.fn(f"run_{op_name}.out")
+            if os.path.exists(log_file):
+                with open(log_file, "r") as f:
+                    lines = f.readlines()
+                    if lines:
+                        last_line = lines[-1].strip()
+                        if "Fri" in last_line:
+                            print(last_line)
+                            print(f"Job {job.id}")
+            else:
+                print(f"Job {job.id} Log file does not exist.")
 print(f"Total unfinished jobs: {count}/{count_running}")
